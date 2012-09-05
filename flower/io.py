baf4f213915922e31b5b6bc85895704609be7a16
benoitc <bchesneau@gmail.com>
2012-09-05 12:49:04 +0200

    add io channel.
    Simple channel to receive READ or WRITE events from any file descriptor.
    
    Internally this channel is using pyuv.Poll. It aims to replace any
    select call in the flower world.

---
+++ flower/io.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -
+#
+# This file is part of flower.
+# See the NOTICE for more information.
+
+import pyuv
+from flower import stackless
+
+UV_ALL = pyuv.UV_READABLE | pyuv.UV_WRITABLE
+UV_READABLE = pyuv.UV_READABLE
+UV_WRITABLE = pyuv.UV_WRITABLE
+
+
+class IOChannel(stackless.channel):
+
+    def __init__(self, io, events=UV_ALL, label=''):
+        super(IOChannel, self).__init__(label=label)
+        if not isinstance(io, int):
+            if hasattr(io, 'fileno'):
+                if callable(io.fileno):
+                    io = io.fileno()
+                else:
+                    io = io.fileno
+            else:
+                raise ValueError("invalid file descriptor number")
+
+        self.io = io
+        self._poller = pyuv.Poll(stackless.get_loop(), io)
+        self._poller.start(events, self._tick)
+
+    def _tick(self, handle, events, errno):
+        if errno:
+            self.send_exception(IOError, errno)
+        else:
+            self.send(events)
+
+    def stop(self):
+        self._poller.stop()
+        self.close()
---
+++ test/test_io.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -
+#
+# This file is part of flower. See the NOTICE for more information.
+
+import os
+import tempfile
+
+from py.test import skip
+from flower import stackless
+from flower.io import IOChannel, UV_READABLE
+
+class Test_IO:
+
+    def test_readable(self):
+        (r, w) = os.pipe()
+
+        ret = []
+        def _read(fd):
+            c = IOChannel(r, events=UV_READABLE)
+            c.receive()
+            ret.append(os.read(fd, 10))
+            c.stop()
+
+        def _write(fd):
+            os.write(fd, "TEST")
+
+        stackless.tasklet(_read)(r)
+        stackless.tasklet(_write)(w)
+        stackless.run()
+
+        assert ret == ['TEST']
