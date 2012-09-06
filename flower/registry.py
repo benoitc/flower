049df38d0e0a8a182846659ccfa2dd05f27ab28a
benoitc <bchesneau@gmail.com>
2012-09-06 14:48:40 +0200

    add an actor registry.
    This registry will be used to register actors. All `Registry` instance
    share the same context.

---
+++ flower/registry.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -
+#
+# This file is part of flower. See the NOTICE for more information.
+
+import operator
+import threading
+from weakref import WeakKeyDictionary, WeakValueDictionary
+
+import six
+
+from flower import stackless
+
+class Registry(object):
+    """ actors registry. This rgistry is used to keep a trace of created
+    actors """
+
+    # share state between instances
+    __shared_state__ = dict(
+            _actors = WeakValueDictionary(),
+            _registered_names = {},
+            _names_by_id = {}
+    )
+
+    def __init__(self):
+        self.__dict__ = self.__shared_state__
+        self._lock = threading.RLock()
+
+
+    def insert(self, actor=None):
+        """ insert an actor instance in the registery """
+        if actor is None:
+            actor = stackless.getcurrent()
+
+        actor_id = id(actor)
+        self._actors[actor_id] = actor
+        return actor_id
+
+    def remove(self, actor=None):
+        """ remove an actor from the registery """
+        if actor is None:
+            actor = stackless.getcurrent()
+            actor_id = id(actor)
+        elif not isinstance(actor, int):
+            actor_id = id(actor)
+        else:
+            actor_id = actor
+
+        del self._actors[actor_id]
+
+        # remove the actor from registered names as well
+        try:
+            names = self._names_by_id.pop(actor_id)
+            for n in names:
+                del self._registered_names[n]
+        except KeyError:
+            pass
+
+    def register(self, name, actor=None):
+        """ register an actor id with a name in the registry """
+        if actor is None:
+            actor = stackless.getcurrent()
+            actor_id = id(actor)
+        elif not isinstance(actor, int):
+            actor_id = id(actor)
+        else:
+            actor_id = actor
+
+        with self._lock:
+            if actor_id not in self._actors:
+                raise KeyError("Actor with %s id is unknown" % actor_id)
+
+            if name in self._registered_names:
+                if self._registered_names[name] == actor_id:
+                    return
+                raise KeyError("An actor is already registered for this name")
+
+            self._registered_names[name] = actor_id
+            if not actor_id in self._names_by_id:
+                self._names_by_id[actor_id] = []
+            self._names_by_id[actor_id].append(name)
+
+    def unregister(self, name):
+        """ unregister a name in the registery """
+
+        if isinstance(name, int):
+            names = self._names_by_id[name]
+            with self._lock:
+                for n in names:
+                    del self._registered_names[n]
+                del self._names_by_id[name]
+        else:
+            actor_id = self._registered_names[name]
+            names  = self._names_by_id[actor_id]
+            del self._registered_names[name]
+            del names[operator.indexOf(names, name)]
+
+    def by_id(self, actor):
+        """ get an actor by it's id """
+        try:
+            return self._actors[actor]
+        except KeyError:
+            raise KeyError("Actor with %s id is unknown" % actor)
+
+    def by_name(self, name):
+        """ get an actor by name """
+        try:
+            return self.by_id(self._registered_names[name])
+        except KeyError:
+            raise KeyError("Actor with %s name is unknown" % name)
+
+    def __getitem__(self, actor):
+        if isinstance(actor, six.string_types):
+            return self.by_name(actor)
+        else:
+            return self.by_id(actor)
+
+    def __delitem__(self, actor):
+        if isinstance(actor, six.string_types):
+            actor = self._registered_names[actor]
+        self.remove(actor)
+
+    def __contains__(self, actor):
+        with self._lock:
+            if isinstance(actor, six.string_types):
+                return actor in self._registered_names
+            else:
+                return actor in self._actors
+
+    def __iter__(self):
+        for actor in self._actors:
+            yield self._actors[actor]
---
+++ test/test_registry.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -
+#
+# This file is part of flower. See the NOTICE for more information.
+
+import pytest
+
+from flower.registry import Registry
+from flower import stackless
+
+class Test_Registry:
+
+    def test_simple(self):
+        r = Registry()
+
+        class _Dummy(object):
+            pass
+
+        obj = _Dummy()
+        oid = id(obj)
+
+        ret_oid = r.insert(obj)
+
+        assert ret_oid == oid
+        assert ret_oid in r
+        assert obj is r[oid]
+
+        r.remove(ret_oid)
+        assert ret_oid not in r
+
+    def test_share_registry(self):
+        r = Registry()
+        r1 = Registry()
+
+        class _Dummy(object):
+            pass
+
+        obj = _Dummy()
+        ret_oid = r.insert(obj)
+
+        assert ret_oid in r1
+        r.remove(ret_oid)
+
+        assert ret_oid not in r
+        assert ret_oid not in r1
+
+    def test_multiple(self):
+        r = Registry()
+
+        class _Dummy(object):
+            pass
+
+        o1 = _Dummy()
+        id1 = id(o1)
+        o2 = _Dummy()
+        id2 = id(o2)
+
+        assert o1 is not o2
+        assert id1 != id2
+
+        r.insert(o1)
+        r.insert(o2)
+
+        assert id1 in r
+        assert id2 in r
+
+        r_list = list(r)
+
+        assert len(r_list) == 2
+        assert o1 in r_list
+        assert o2 in r_list
+
+        r.remove(id1)
+        assert id1 not in r
+        assert id2 in r
+
+        r.remove(id2)
+        assert id2 not in r
+
+    def test_register(self):
+        r = Registry()
+
+        class _Dummy(object):
+            pass
+
+        o1 = _Dummy()
+        id1 = id(o1)
+        o2 = _Dummy()
+        id2 = id(o2)
+
+        ret_id1 = r.insert(o1)
+        assert ret_id1 == id1
+        assert id1 in r
+
+        r.register("test", ret_id1)
+        assert "test" in r
+        assert r["test"] is o1
+
+        with pytest.raises(KeyError):
+            r.register("test2", id2)
+
+        r.insert(o2)
+        r.register("test2", id2)
+        assert "test2" in r
+
+        r.unregister("test")
+        assert "test" not in r
+
+        r.register("test", ret_id1)
+        assert "test" in r
+
+
+        with pytest.raises(KeyError):
+            r.register("test", id2)
+
+        r.unregister(id1)
+        assert "test" not in r
+
+        del r["test2"]
+        assert id2 not in r
+
+        del r[id1]
+
+
+    def test_register_current(self):
+        r = Registry()
+
+        actor = stackless.getcurrent()
+        with pytest.raises(KeyError):
+            r.register("test")
+
+
+        actor_id = r.insert(stackless.getcurrent())
+        r.register("test")
+        assert "test" in r
+        assert r["test"] is stackless.getcurrent()
+
+        del r['test']
+
+
+
+
+
