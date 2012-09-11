2dc280d6b4feefb5bf5f1321436eb01f76834a1c
benoitc <bchesneau@gmail.com>
2012-09-11 21:46:38 +0200

    missing file.

---
+++ flower/net/util.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -
+#
+# This file is part of flower. See the NOTICE for more information.
+
+import socket
+
+def is_ipv6(addr):
+    try:
+        socket.inet_pton(socket.AF_INET6, addr)
+    except socket.error: # not a valid address
+        return False
+    return True
+
+def parse_address(netloc, default_port=0):
+    if isinstance(netloc, tuple):
+        return netloc
+
+    # get host
+    if '[' in netloc and ']' in netloc:
+        host = netloc.split(']')[0][1:].lower()
+    elif ':' in netloc:
+        host = netloc.split(':')[0].lower()
+    elif netloc == "":
+        host = "0.0.0.0"
+    else:
+        host = netloc.lower()
+
+    #get port
+    netloc = netloc.split(']')[-1]
+    if ":" in netloc:
+        port = netloc.split(':', 1)[1]
+        if not port.isdigit():
+            raise RuntimeError("%r is not a valid port number." % port)
+        port = int(port)
+    else:
+        port = default_port
+    return (host, port)
+
